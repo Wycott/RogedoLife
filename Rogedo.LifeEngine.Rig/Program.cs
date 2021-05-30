38bba4e1b7513369a87978f30533ba965095b6d5
Rob Docherty <robert.g.docherty@gmail.com>
2021-05-30 23:45:31 +0100

    Stop flicker in rig

--- Rogedo.LifeEngine.Rig/Program.cs
+++ Rogedo.LifeEngine.Rig/Program.cs
@@ -8,7 +8,7 @@ namespace Rogedo.LifeEngine.Rig
     {
         static void Main(string[] args)
         {
-            const int dimension = 60;
+            const int dimension = 50;
 
             //IArena gameArena = new Arena();
             //gameArena.Initialise(dimension);
@@ -19,15 +19,18 @@ namespace Rogedo.LifeEngine.Rig
             //gameArena.Seed(2, 2);
 
             IArena gameArena = new Arena();
-            gameArena.InitialiseRandomly(dimension);            
+            gameArena.InitialiseRandomly(dimension);
 
+            Console.Clear();
+            Console.CursorVisible = false;
             while (gameArena.GetPopulation() > 0 && !gameArena.Repeating)
             {
-                Console.Clear();
+                //Console.Clear();
                 PrintArena(gameArena, dimension);
                 gameArena.MakeNextGeneration();
-                System.Threading.Thread.Sleep(250);                
-            }                       
+                //System.Threading.Thread.Sleep(250);                
+            }
+            Console.CursorVisible = true;
         }
 
         static void PrintArena(IArena arena, int dimension)
@@ -35,10 +38,29 @@ namespace Rogedo.LifeEngine.Rig
             ConsoleColor defaultColour = Console.ForegroundColor;
 
             int currentCell = 0;
-
+            int x = 0;
+            int y = 0;
             foreach (var c in arena.ArenaCells)
             {
-                currentCell++;
+                //currentCell++;
+                //if (c.Generation == Interfaces.Types.CellGeneration.Dead)
+                //{
+                //    Console.ForegroundColor = defaultColour;
+                //    Console.Write(" .");
+                //}
+                //else
+                //{
+                //    Console.ForegroundColor = ConsoleColor.Yellow;
+                //    Console.Write(" o");
+                //    Console.ForegroundColor = defaultColour;
+                //}
+
+                //if (currentCell % dimension == 0)
+                //    Console.WriteLine();                
+
+                x = (currentCell % dimension) * 2;
+                y = currentCell / dimension;
+                Console.SetCursorPosition(x, y);
                 if (c.Generation == Interfaces.Types.CellGeneration.Dead)
                 {
                     Console.ForegroundColor = defaultColour;
@@ -47,17 +69,18 @@ namespace Rogedo.LifeEngine.Rig
                 else
                 {
                     Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.Write(" o");
+                    Console.Write(" O");
                     Console.ForegroundColor = defaultColour;
                 }
+                currentCell++;
 
-                if (currentCell % dimension == 0)
-                    Console.WriteLine();                
             }
 
             Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(0, dimension );
             Console.WriteLine($"Generation: {arena.GetGeneration()}");
-            Console.WriteLine($"Population: {arena.GetPopulation()}");
+            Console.SetCursorPosition(0, dimension + 1);
+            Console.WriteLine($"Population: {arena.GetPopulation()}         ");
             Console.ForegroundColor = defaultColour;
         }
 
